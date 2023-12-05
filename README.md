ae1376175f197bae6aabf52d13fde50a60f381ea
fdfdjfk3 <gdperin32@gmail.com>
2023-12-04 21:06:55 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# tagged_union.h
+
+I wanted to see if it was possible to implement some kind of 'safe' tagged union
+data type in pure C, and here's the result. It's... uh... far from perfect,
+but it's close enough lol. This header relies on some boost preprocessor
+headers because I don't wanna end up in a mental institution trying to
+hand-roll my own macro trickery :P.
+
+Included macros:
+
+```c
+tagged_union(name, pairs)
+/// Macro that expands to a tagged union declaration; which is 
+/// a struct with an enum field containing one value for each field in 
+/// the union, and the union itself, with the fields named after each
+/// member of the enum.
+```
+```c
+tagged_union_from_enum(name, enumtype, pairs)
+/// Like tagged_union, but it uses a given enum type as the backing enum.
+```
+```c
+TUnionGet(t_union, variant, type)
+/// Gets the value in the tagged union. If it's an inactive field, an error is 
+/// issued and the program exits.
+```
+```c
+TUnionLit(variant, literal)
+/// Expands to a compound literal of a tagged union. Use this instead of 
+/// manually writing out compound literals to avoid silly mistakes.
+/// literals to avoid silly mistakes.
+```
+```c
+TUnionActive(t_union)
+/// Expands to the active field enum
+```
+```c
+TUnionSet(t_union, variant, value)
+/// Modifies the current variant's value. If the variant is different
+/// than the one stored, it will be an error.
+```
+
+## Example usage:
+
+```c
+#include "tagged_unions.h"
+#include <stdio.h>
+
+typedef tagged_union(test, (
+	(THING1, int),
+	(THING2, struct { int bla_bla; }),
+	(THING3, float),
+	(THING4, struct test *),
+	(THING5, wchar_t)
+)) test;
+
+int main(void) {
+	test a = TUnionLit(THING3, 5.5);
+	printf("%f\n", TUnionGet(a, THING3, float));
+}
+```
+
+## Closure
+
+ok bai
---
+++ tagged_union.h
@@ -0,0 +1,93 @@
+#include <boost/preprocessor/tuple.hpp>
+#include <boost/preprocessor/inc.hpp>
+#include <boost/preprocessor/for.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define _TU_ARR_ITER_PRED(r, state) \
+	BOOST_PP_NOT_EQUAL(	\
+		BOOST_PP_TUPLE_ELEM(2, 0, state), \
+		BOOST_PP_TUPLE_SIZE( \
+			BOOST_PP_TUPLE_ELEM(2, 1, state) \
+		) \
+	) \
+
+#define _TU_ARR_INC(r, state) \
+	( \
+		BOOST_PP_INC(BOOST_PP_TUPLE_ELEM(2, 0, state)), \
+		BOOST_PP_TUPLE_ELEM(2, 1, state) \
+	) \
+
+#define _TU_SETUP_ENUM(r, state) \
+	BOOST_PP_TUPLE_ELEM( \
+		0, \
+		BOOST_PP_TUPLE_ELEM( \
+			BOOST_PP_TUPLE_ELEM(0, state), \
+			BOOST_PP_TUPLE_ELEM(1, state) \
+		) \
+	) , \
+
+#define _TU_SETUP_UNION(r, state) \
+	BOOST_PP_TUPLE_ELEM( \
+		1, \
+		BOOST_PP_TUPLE_ELEM( \
+			BOOST_PP_TUPLE_ELEM(0, state), \
+			BOOST_PP_TUPLE_ELEM(1, state) \
+		) \
+	) \
+	BOOST_PP_TUPLE_ELEM( \
+		0, \
+		BOOST_PP_TUPLE_ELEM( \
+			BOOST_PP_TUPLE_ELEM(0, state), \
+			BOOST_PP_TUPLE_ELEM(1, state) \
+		) \
+	) \
+	; \
+
+/// Macro that expands to a tagged union declaration; which is 
+/// a struct with an enum field containing one value for each field in 
+/// the union, and the union itself, with the fields named after each
+/// member of the enum.
+#define tagged_union(name, pairs) \
+	struct name { \
+		enum { \
+			BOOST_PP_FOR((0, pairs), _TU_ARR_ITER_PRED, _TU_ARR_INC, _TU_SETUP_ENUM) \
+		} active; \
+		union { \
+			BOOST_PP_FOR((0, pairs), _TU_ARR_ITER_PRED, _TU_ARR_INC, _TU_SETUP_UNION)\
+		}; \
+	}
+
+/// Like tagged_union, but it uses a given enum type as the backing enum.
+#define tagged_union_from_enum(name, enumtype, pairs) \
+	struct name { \
+		enumtype active; \
+		union { \
+			BOOST_PP_FOR((0, pairs), _TU_ARR_ITER_PRED, _TU_ARR_INC, _TU_SETUP_UNION)\
+		}; \
+	}
+
+void *_t_union_check_error(const char *file, int line) {
+	printf("%s:%d: Tagged union safety error: attempted to access inactive field.\n", file, line);
+	exit(EXIT_FAILURE);
+}
+
+/// Gets the value in the tagged union. If it's an inactive field, an error is 
+/// issued and the program exits.
+#define TUnionGet(t_union, variant, type) ((t_union.active == variant) ? t_union.variant : *(type *)_t_union_check_error(__FILE__, __LINE__))
+
+/// Expands to a compound literal of a tagged union. Use this instead of 
+/// manually writing out compound literals to avoid silly mistakes.
+/// literals to avoid silly mistakes.
+#define TUnionLit(variant, literal) {.active = variant, .variant = literal}
+
+/// Expands to the active field enum
+#define TUnionActive(t_union) t_union.active
+
+/// Modifies the current variant's value. If the variant is different
+/// than the one stored, it will be an error.
+#define TUnionSet(t_union, variant, value) \
+	if (t_union.active != variant) { \
+		_t_union_check_error(__FILE__, __LINE__) \
+	} \
+	t_union.variant = value;
